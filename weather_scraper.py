bf9cffe946d6510c3d1106744d2a4c058e05ee58
caleanunoah <43590970+caleanunoah@users.noreply.github.com>
2019-06-03 21:24:15 -0700

    url in correct form now
    can start scraping info for user input location

--- weather_scraper.py
+++ weather_scraper.py
@@ -3,32 +3,36 @@ from bs4 import BeautifulSoup
 import re
 from pprint import pprint as p
 
-page = requests.get('https://www.theweathernetwork.com/ca')
-soup = BeautifulSoup(page.content, 'html.parser')
-province = input('Enter a Canadian Province: ')
-city = input("Enter a city in provinc: ")
-adjust_prov(province)
-url = "https://www.theweathernetwork.com/ca/weather/"+province+"/"+city
-p(url)
-
-
-#  Fix the province to account for strings with spaces.
-#   Replace " " (spaces) with "-" (hyphens)
-def fix_string(province):
-    pass
 
 class Locators:
     """
     Locators for items in the HTML page
     """
     CITY_LOCATOR = 'body div.'
-    TEMP_LOCATOR = 'article.product_pod h3 a'
+    TEMP_LOCATOR = ''
+
+
+def form_url(prov, city):
+    pattern = '\s+'
+    new_prov = re.sub(pattern, "-", prov)
+    new_city = re.sub(pattern, "-", city)
+    url = "https://www.theweathernetwork.com/ca/weather/" + new_prov.lower()+ "/" + new_city.lower()
+    p(url)
 
 
 def find_city():
-    locator = Locators.CITY_LOCATOR
-    city = soup.select_one(locator)
-    print(city)
+    #locator = Locators.CITY_LOCATOR
+    #city = soup.select_one(locator)
+    #print(city)
+    pass
+
+
+#page = requests.get('https://www.theweathernetwork.com/ca')
+#soup = BeautifulSoup(page.content, 'html.parser')
+province = input('Enter a Canadian Province: ')
+city = input("Enter a city in province: ")
+form_url(province, city)
+
 
 
 
