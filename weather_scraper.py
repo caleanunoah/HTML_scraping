ac5efdc59a08e1c8b0b7b06566113387f6ae68b5
caleanunoah <43590970+caleanunoah@users.noreply.github.com>
2019-06-07 18:33:13 -0700

    scraping i.
    using requests.get(..., 'html.parser').contents

--- weather_scraper.py
+++ weather_scraper.py
@@ -1,37 +1,74 @@
 import requests
-from bs4 import BeautifulSoup
 import re
+
+from bs4 import BeautifulSoup
 from pprint import pprint as p
+
+
+#  Fix the province to account for strings with spaces.
+#   Replace " " (spaces) with "-" (hyphens)
+def fix_strings(province, city):
+    pattern = '\s'
+    adj_prov = re.sub(pattern, '-', province)
+    adj_city = re.sub(pattern, '-', city)
+    url = "https://www.theweathernetwork.com/ca/weather/"+adj_prov.lower()+"/"+adj_city.lower()
+    p(url)
+    return url
 
 
 class Locators:
     """
     Locators for items in the HTML page
     """
-    CITY_LOCATOR = 'body div.'
-    TEMP_LOCATOR = ''
+    CITY_LOCATOR = 'body'
+    #TEMP_LOCATOR = 'body div.content-wrap div.clearfix div.o-main-content div.module div.wx-content.current-obs div.current-wx-obs.override-margin div.wx-info_card div.obs-area span.temp'
+    TEMP_LOCATOR = 'body div.content-wrap div.clearfix div.o-main-content'
 
 
-def form_url(prov, city):
-    pattern = '\s+'
-    new_prov = re.sub(pattern, "-", prov)
-    new_city = re.sub(pattern, "-", city)
-    url = "https://www.theweathernetwork.com/ca/weather/" + new_prov.lower()+ "/" + new_city.lower()
-    p(url)
+class WeatherPage:
+    """
+    This returns the container where all weather info is extracted from.
+    """
+    def __init__(self, page):
+        self.soup = BeautifulSoup(page, 'lxml')
 
+    def weather(self):
+        locator = Locators.TEMP_LOCATOR
+        #city = self.soup.select(locator)
+        data= self.soup.find("span", attrs={"class": "wxcondition", })
+        return data[0]
+        #return self.soup.select_one(locator).prettify()
+        #return self.soup.select_one(locator)
+        #return WeatherParser(city)
 
-def find_city():
+'''
+class WeatherParser:
+"""
+
+"""
+    def __init__(self, parent):
+        self.parent = parent            # THIS IS NONE
+
+    def __repr__(self):
+        return f'<The weather in {self.parent} is X'    # PRINTS EMPTY LIST BC PARENT IS NONE!!!!
+
+    @property
+    def temp(self):
+        temp_locator = Locators.TEMP_LOCATOR
+        return self.parent.select_one(temp_locator)
+'''
+
+if __name__ == '__main__':
+    #province = input('Enter a Canadian province: ')
+    #City = input("Enter a city in province: ")
+    #URL = fix_strings(province, City)
+    URL = 'https://www.theweathernetwork.com/ca/36-hour-weather-forecast/british-columbia/surrey'
+    page_content = requests.get(URL).content
+    Weather = WeatherPage(page_content)
+    p("Im done scraping. ")
+    p(Weather.weather())
     #locator = Locators.CITY_LOCATOR
     #city = soup.select_one(locator)
-    #print(city)
-    pass
-
-
-#page = requests.get('https://www.theweathernetwork.com/ca')
-#soup = BeautifulSoup(page.content, 'html.parser')
-province = input('Enter a Canadian Province: ')
-city = input("Enter a city in province: ")
-form_url(province, city)
 
 
 
